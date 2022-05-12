2e944454144f4edc5288f497302549d63c1512e3
Kaylee Dao <kayleed2@gmail.com>
2022-05-12 17:00:21 -0500

    Adding main.ts for task 10

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,9 @@
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher = {
+    firstName: 'John',
+    lastName: 'Smith',
+    experienceTeachingC: 10,
+}
