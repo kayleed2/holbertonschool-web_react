8ae3012ab219e637b0c6705b9b2b3f651ead053c
Kaylee Dao <kayleed2@gmail.com>
2023-01-07 10:35:52 -0600

    Task 1

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import logo from './holbertonlogo.jpg';
+import { getFullYear } from './utils.js';
+import { getFooterCopy } from './utils.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>{getFullYear()} - {getFooterCopy(0)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
