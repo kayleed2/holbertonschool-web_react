008d857a5e3ee643082d50393905f2f9867c0aed
Kaylee Dao <kayleed2@gmail.com>
2023-01-18 13:01:25 -0600

    With const

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -6,16 +6,23 @@ CourseListRow.propTypes = {
     isHeader: PropTypes.bool,
     textFirstCell: PropTypes.string.isRequired,
     textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  };
+};
   
   CourseListRow.defaultProps = {
     isHeader: false,
     textSecondCell: null,
-  };
-  
+};
+
+const styles = StyleSheet.create({
+    content: {
+      border: '1px solid #e1354b',
+    },
+});
+
+
 export default function CourseListRow({ isHeader, textFirstCell, textSecondCell} ) {
     return (
-    <tr style={{ backgroundColor: isHeader ? '#deb5b545' : '#f5f5f5ab' }}>
+    <tr className={css(styles.content)} style={{ backgroundColor: isHeader ? '#deb5b545' : '#f5f5f5ab' }}>
         {isHeader && textSecondCell === null && (
             <th colSpan={2}>{textFirstCell}</th>
         )}
