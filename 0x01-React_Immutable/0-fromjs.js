9236463840b4fac9b2251a0170d00eac91b9f6e8
Kaylee Dao <kayleed2@gmail.com>
2023-01-24 10:46:29 -0600

    1-2

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -1,5 +1,6 @@
-import { fromJS } from "immutable";
+import { fromJS } from 'immutable';
 
-export default function getImmutableObject({ object }) {
-    return fromJS(object);
+
+export default function getImmutableObject(object) {
+  return fromJS(object);
 }
